b86a58806c6d54a807c67c8d7741b48e5e877867
k5263 <59162642+k5263@users.noreply.github.com>
2022-10-16 19:28:29 +0900

    Add files via upload

---
+++ class1.java
@@ -0,0 +1,8 @@
+package package1;
+
+public class class1 {
+	public static void main(String[] args) {
+		class2 a = new class2();
+		System.out.print(a.re(5));
+	}
+}
---
+++ class2.java
@@ -0,0 +1,7 @@
+package package1;
+
+public class class2 {
+	public int re(int b) {
+		return b;
+	}
+}
